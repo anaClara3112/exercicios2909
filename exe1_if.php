9f6d1a4eeac7b92f5c8a142a0e056ce455af2ac3
anaClara3112 <130669098+anaClara3112@users.noreply.github.com>
2023-09-29 11:15:44 -0300

    commit ana

--- exe1_if.php
+++ exe1_if.php
@@ -10,7 +10,6 @@ echo "O resultado é verdadeiro!";
 $nome = 'Robson';
 $nome1 = 'Pedro';
 
-//$nome = $nome != $nome1;
 if ($nome != $nome1){
 echo "O resultado é diferente!";
 }
@@ -18,7 +17,6 @@ echo "O resultado é diferente!";
 $a = 12;
 $b = 11;
 
-//$calculo = $a <= $b;
 if ($a <= $b){ 
 echo "O resultado é verdadeiro!";
 }
---
+++ exe3.php
@@ -0,0 +1,36 @@
+<?php
+
+//Variáveis com números 
+$a = 60;
+$b = 20;
+//Variável com string
+$c = "30";
+
+//A variável é um número
+if(is_numeric($a));
+   $result= $a *2 ; 
+    if ($result > 100) {
+        echo "Afirmação verdadeira <br> ";
+    } else {
+        echo "Afirmação falsa <br> ";
+    }
+
+if(is_numeric($b));
+    $result= $b *2 ; 
+     if ($result > 100) {
+         echo "Afirmação verdadeira <br> ";
+     } else {
+         echo "Afirmação falsa <br> ";
+     }
+
+     if(is_numeric($c));
+   $result= $c *2 ; 
+    if ($result > 100) {
+        echo "Afirmação verdadeira <br> ";
+    } else {
+        echo "Afirmação falsa <br> ";
+    }
+?>
+
+
+
