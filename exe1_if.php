f9851535e72a7ed0a7a6e5ee195b5b2038848441
anaClara3112 <130669098+anaClara3112@users.noreply.github.com>
2023-09-29 09:09:55 -0300

    commit da ana

--- exe1_if.php
+++ exe1_if.php
@@ -5,18 +5,27 @@ $b = 2;
 
 $calculo = $a > $b;
 echo "<p>" . $calculo . "Verdadeiro!</p>";
+if ($a > $b) {
+
+}
 
 $nome = 'Robson';
 $nome1 = 'Pedro';
 
 $nome = $nome != $nome1;
 echo "<p>" . $nome . "Diferente!</p>";
+if ($nome != $nome1) {
+    
+}
 
 $a = 12;
 $b = 11;
 
 $calculo = $a <= $b;
 echo "<p>" . $calculo . "Verdadeiro!</p>";
+if ($a <= $b) {
+    
+}
 
 
 ?>
