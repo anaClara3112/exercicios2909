06208db20daae868af513f0e639fb758e12440c2
anaClara3112 <130669098+anaClara3112@users.noreply.github.com>
2023-09-29 08:55:41 -0300

    commit da ana

---
+++ exe1.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 5;
+$b = 2;
+
+$calculo = $a > $b;
+echo "<p>" . $calculo . "Verdadeiro!</p>";
+
+$nome = 'Robson';
+$nome1 = 'Pedro';
+
+$nome = $nome != $nome1;
+echo "<p>" . $nome . "Diferente!</p>";
+
+$a = 12;
+$b = 11;
+
+$calculo = $a <= $b;
+echo "<p>" . $calculo . "Verdadeiro!</p>";
+
+
+?>
+
